a249350f61ade2113efdfb3b229a00533a31ed4b
Deepanker5 <kanjarladeepanker@gmail.com>
2025-06-11 15:44:49 +0300

    explored training data

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
 import tiktoken
 
-f = open("train.txt", "r")
-tiktoken.encoding_for_model("")
+f = open("GPT-2-c/train.txt", "r")
+enc = tiktoken.encoding_for_model("gpt2")
+tokens = enc.encode(f.read())
+text = enc.decode(tokens)
+print(text)
